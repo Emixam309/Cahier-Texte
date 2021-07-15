81715c957f4b775a0c6e9e9b916d34d996b70f0b
Emixam <crmaxime@gmail.com>
2021-07-15 19:23:25 +0200

    final commit

--- admin/compte-rendu.php
+++ admin/compte-rendu.php
@@ -1,84 +1,1 @@
-<?php include("session.php");
-if (isset($_POST['compte-rendu'])) { //update du compte rendu sélectionné
-    $query = 'UPDATE compterendu SET idModule=?, idPromo=?, idUser=?, date=?, duree=?, distanciel=?, contenu=?, moyen=?, objectif=?, evaluation=?
-              WHERE idCompteRendu =' . $_POST['idCR'];
-    $stmt = $bdd->prepare($query);
-    $stmt->bind_param("iiisiissss", $_POST['module'], $_POST['promotion'], $_SESSION['idUser'], $_POST['date'], $_POST['duree'],
-        $_POST['distanciel'], $_POST['contenu'], $_POST['moyen'], $_POST['objectif'], $_POST['evaluation']);
-    $stmt->execute();
-    header("Location: {$_POST['header']}");
-}
-if (isset($_POST['idCR'])) { //requete pour afficher le contenu a modifier
-    $query = $bdd->query('SELECT * FROM compterendu WHERE idCompteRendu = "' . $_POST['idCR'] . '"');
-    $resultEdit = $query->fetch_object();
-    $query->close();
-}
-?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <title>Cahier de Texte</title>
-</head>
-<body>
-<?php include("navbar.php") ?>
-<div class="container">
-    <h1 class="text-center mb-3">Modifier le Compte Rendu de la séance</h1>
-    <form class="row" action="" method="post" name="compte-rendu">
-        <div class="col-lg-auto ms-auto">
-            <div class="form-floating mb-3">
-                <input class="form-control" name="date"
-                       type="date" <?php if (isset($_POST['idCR'])) echo 'value="' . $resultEdit->date . '"' ?>
-                       required>
-                <label for="date">Date</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input class="form-control" name="duree" type="number"
-                       placeholder="Durée" <?php if (isset($_POST['idCR'])) echo 'value="' . $resultEdit->duree . '"' ?>>
-                <label for="duree">Durée de la séance</label>
-            </div>
-            <div class="form-check mb-3">
-                <input class="form-check-input" type="checkbox" id="distanciel" name="distanciel"
-                       value="1" <?php if (isset($_POST['idCR'])) if (isset($resultEdit->distanciel)) echo "checked" ?>>
-                <label class="form-check-label" for="distanciel">Distanciel</label>
-            </div>
-        </div>
-        <div class="col-lg-5 me-auto">
-            <div class="form-floating mb-3">
-                        <textarea class="form-control" placeholder="Contenu de la séance" name="contenu"
-                                  style="height: 100px"
-                                  maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->contenu ?></textarea>
-                <label for="contenu">Contenu de la séance</label>
-            </div>
-            <div class="form-floating mb-3">
-                        <textarea class="form-control" placeholder="Moyens techniques et pédagogiques" name="moyen"
-                                  style="height: 100px"
-                                  maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->moyen ?></textarea>
-                <label for="moyen">Moyens techniques et pédagogiques</label>
-            </div>
-            <div class="form-floating mb-3">
-                <textarea class="form-control" placeholder="Objectif Ciblé" name="objectif"
-                          maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->objectif ?></textarea>
-                <label for="objectif">Objectif ciblé (référentiel)</label>
-            </div>
-            <div class="form-floating mb-3">
-                <textarea class="form-control" placeholder="Evaluation" name="evaluation"
-                          maxlength="250"><?php if (isset($_POST['idCR'])) echo $resultEdit->evaluation ?></textarea>
-                <label for="evaluation">Evaluation (si il y a, indiquer le libellé)</label>
-            </div>
-        </div>
-        <div class="text-center">
-            <input type="hidden" value="<?php echo $_SERVER['HTTP_REFERER'] ?>" name="header">
-            <input type="hidden" value="<?php echo $_POST['idCR'] ?>" name="idCR">
-            <input type="hidden" value="<?php echo $_POST['promotion'] ?>" name="promotion">
-            <input type="hidden" value="<?php echo $_POST['module'] ?>" name="module">
-            <input class="btn btn-primary" type="submit" value="Modifier" name="compte-rendu">
-        </div>
-    </form>
-</div>
-</body>
-</html>
+<?php include("session.php");if (isset($_POST['compte-rendu'])) { //update du compte rendu sélectionné    $query = 'UPDATE compterendu SET idModule=?, idPromo=?, idUser=?, date=?, duree=?, distanciel=?, contenu=?, moyen=?, objectif=?, evaluation=?              WHERE idCompteRendu =' . $_POST['idCR'];    $stmt = $bdd->prepare($query);    $stmt->bind_param("iiisiissss", $_POST['module'], $_POST['promotion'], $_SESSION['idUser'], $_POST['date'], $_POST['duree'],        $_POST['distanciel'], $_POST['contenu'], $_POST['moyen'], $_POST['objectif'], $_POST['evaluation']);    $stmt->execute();    header("Location: {$_POST['header']}");}if (isset($_POST['idCR'])) { //requete pour afficher le contenu a modifier    $query = $bdd->query('SELECT * FROM compterendu WHERE idCompteRendu = "' . $_POST['idCR'] . '"');    $resultEdit = $query->fetch_object();    $query->close();}?><!doctype html><html lang="fr"><head>    <meta charset="UTF-8">    <meta name="viewport"          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">    <meta http-equiv="X-UA-Compatible" content="ie=edge">    <link href="../css/bootstrap.css" rel="stylesheet">    <title>Cahier de Texte</title></head><body><?php include("navbar.php") ?><div class="container">    <h1 class="text-center mb-3">Modifier le Compte Rendu de la séance</h1>    <form class="row" action="" method="post" name="compte-rendu">        <div class="col-lg-auto ms-auto">            <div class="form-floating mb-3">                <input class="form-control" name="date"                       type="date" <?php if (isset($_POST['idCR'])) echo 'value="' . $resultEdit->date . '"' ?>                       required>                <label for="date">Date</label>            </div>            <div class="form-floating mb-3">                <input class="form-control" name="duree" type="number"                       placeholder="Durée" <?php if (isset($_POST['idCR'])) echo 'value="' . $resultEdit->duree . '"' ?>>                <label for="duree">Durée de la séance</label>            </div>            <div class="form-check mb-3">                <input class="form-check-input" type="checkbox" id="distanciel" name="distanciel"                       value="1" <?php if (isset($_POST['idCR'])) if (isset($resultEdit->distanciel)) echo "checked" ?>>                <label class="form-check-label" for="distanciel">Distanciel</label>            </div>        </div>        <div class="col-lg-5 me-auto">            <div class="form-floating mb-3">                        <textarea class="form-control" placeholder="Contenu de la séance" name="contenu"                                  style="height: 100px"                                  maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->contenu ?></textarea>                <label for="contenu">Contenu de la séance</label>            </div>            <div class="form-floating mb-3">                        <textarea class="form-control" placeholder="Moyens techniques et pédagogiques" name="moyen"                                  style="height: 100px"                                  maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->moyen ?></textarea>                <label for="moyen">Moyens techniques et pédagogiques</label>            </div>            <div class="form-floating mb-3">                <textarea class="form-control" placeholder="Objectif Ciblé" name="objectif"                          maxlength="255"><?php if (isset($_POST['idCR'])) echo $resultEdit->objectif ?></textarea>                <label for="objectif">Objectif ciblé (référentiel)</label>            </div>            <div class="form-floating mb-3">                <textarea class="form-control" placeholder="Evaluation" name="evaluation"                          maxlength="250"><?php if (isset($_POST['idCR'])) echo $resultEdit->evaluation ?></textarea>                <label for="evaluation">Evaluation (si il y a, indiquer le libellé)</label>            </div>        </div>        <div class="text-center">            <input type="hidden" value="<?php echo $_SERVER['HTTP_REFERER'] ?>" name="header">            <input type="hidden" value="<?php echo $_POST['idCR'] ?>" name="idCR">            <input type="hidden" value="<?php echo $_POST['promotion'] ?>" name="promotion">            <input type="hidden" value="<?php echo $_POST['module'] ?>" name="module">            <input class="btn btn-primary" type="submit" value="Modifier" name="compte-rendu">        </div>    </form></div></body></html>
--- admin/cr-formation.php
+++ admin/cr-formation.php
@@ -1,163 +1,1 @@
-<?php include("session.php");
-if (isset($_POST['del-compte-rendu'])) {
-    if (!$bdd->query('DELETE FROM compterendu WHERE idCompteRendu = "' . $_POST['del-compte-rendu'] . '"')) {
-        printf("Erreur : %s\n", $bdd->error);
-        $alertDelFail = "Le compte rendu n'a pas pu être supprimé.";
-    } else {
-        $alertDelSuccess = "Le compte rendu a bien été supprimé.";
-    }
-} ?>
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <title>Cahier de Texte</title>
-</head>
-<body>
-<?php include("navbar.php") ?>
-<div class="container">
-    <h1 class="text-center mb-4">Cahier de Texte par Formations</h1>
-    <div class="row">
-        <div class="col-xl-auto mx-auto mb-3">
-            <form class="mb-3" action="" method="get" name="formations">
-                <div class="form-floating mb-3">
-                    <select class="form-select" name="formation" onchange="this.form.submit()">
-                        <option hidden selected>Sélectionner une formation</option>
-                        <?php //Requete + verification formation sélectionnée
-                        $query = $bdd->query('SELECT formations.idFormation, formations.libelle, formations.reference FROM formations
-                        INNER JOIN modules ON formations.idFormation = modules.idFormation
-                        GROUP BY libelle');
-                        while ($resultat = $query->fetch_object()) {
-                            echo '<option value="' . $resultat->idFormation . '"';
-                            if (isset($_GET['formation'])) {
-                                if ($_GET['formation'] == $resultat->idFormation) {
-                                    $libelle = $resultat->reference;
-                                    echo 'selected';
-                                }
-                            }
-                            echo '>' . $resultat->libelle . '</option>';
-                        }
-                        $query->close(); ?>
-                    </select>
-                    <label for="formation">Formation</label>
-                </div>
-            </form>
-            <form class="mb-3" action="" method="get" name="promotions">
-                <?php if (isset($_GET['formation'])) {
-                echo '<input type="hidden" name="formation" value="' . $_GET['formation'] . '">'; ?>
-                <div class="form-floating mb-3">
-                    <select class="form-select" name="promotion" onchange="this.form.submit()">
-                        <option selected value="">Promotions non clôturées</option>
-                        <?php //Requete + verification formation sélectionnée
-                        $query = $bdd->query('SELECT idPromo, libelle FROM promotions WHERE idFormation = ' . $_GET['formation'] . ' ORDER BY verrouillage, dateDebut, dateFin');
-                        while ($resultat = $query->fetch_object()) {
-                            ;
-                            echo '<option value="' . $resultat->idPromo . '"';
-                            if (isset($_GET['promotion'])) {
-                                if ($_GET['promotion'] == $resultat->idPromo) {
-                                    $libelle = $resultat->libelle;
-                                    echo 'selected';
-                                }
-                            }
-                            echo '>' . $resultat->libelle . '</option>';
-                        }
-                        $query->close();
-                        ?>
-                    </select>
-                    <label for="formation">Promotion</label>
-                </div>
-            </form>
-        </div>
-        <?php } if (isset($_GET['formation'])) { ?>
-        <div class="col-xl-auto mx-auto">
-            <?php
-            echo '<h2 class="text-center mb-3">Cahier de Texte de ' . $libelle . '</h2>';
-            ob_start() ?>
-            <table class="table table-striped border border-3 text-center">
-                <thead>
-                <tr>
-                    <th scope="col">Num. Sem.</th>
-                    <th scope="col">Module</th>
-                    <th scope="col">Formateur</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Durée</th>
-                    <th scope="col">Contenu</th>
-                    <th scope="col">Moyen</th>
-                    <th scope="col">Objectif</th>
-                    <th scope="col">Evaluation</th>
-                    <th scope="col">Distanciel</th>
-                    <th scope="col">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                if (empty($_GET['promotion'])) {
-                    $query = $bdd->query('SELECT idCompteRendu, modules.idFormation, promotions.idPromo, modules.idModule, modules.libelle as mLibelle, compterendu.duree, nom, prenom, date, contenu, moyen, objectif, evaluation, distanciel, verrouillage FROM ((compterendu
-                        INNER JOIN modules ON modules.idModule = compterendu.idModule)
-                        INNER JOIN users on users.idUser = compterendu.idUser)
-                        INNER JOIN promotions on compterendu.idPromo = promotions.idPromo
-                        WHERE modules.idFormation = ' . $_GET['formation'] . ' AND verrouillage != 1 ORDER BY date, dateEntree');
-                } else {
-                    $query = $bdd->query('SELECT idCompteRendu, idFormation, idPromo, modules.idModule, modules.libelle as mLibelle, compterendu.duree, nom, prenom, date, contenu, moyen, objectif, evaluation, distanciel FROM ((compterendu
-                        INNER JOIN modules ON modules.idModule = compterendu.idModule)
-                        INNER JOIN users on users.idUser = compterendu.idUser)
-                        WHERE idPromo = ' . $_GET['promotion'] . ' ORDER BY date, dateEntree');
-                }
-                while ($resultat = $query->fetch_object()) {
-                    $date = strtotime($resultat->date); ?>
-                    <tr>
-                        <th scope="row"><?php echo date('W', $date) ?></th>
-                        <td><?php echo $resultat->mLibelle ?></td>
-                        <td><?php echo $resultat->nom . ' ' . $resultat->prenom ?></td>
-                        <td><?php echo date('d/m/Y', $date) ?></td>
-                        <td><?php echo $resultat->duree ?>h</td>
-                        <td><?php echo $resultat->contenu ?></td>
-                        <td><?php echo $resultat->moyen ?></td>
-                        <td><?php echo $resultat->objectif ?></td>
-                        <td><?php echo $resultat->evaluation ?></td>
-                        <td>
-                            <?php
-                            if (!empty($resultat->distanciel)) {
-                                echo '✔';
-                            } ?>
-                        </td>
-                        <td id="a">
-                            <a href="#"
-                               onclick="document.getElementById('edit-cr-<?php echo $resultat->idCompteRendu ?>').submit()">Modifier</a>
-                            <a href="#"
-                               onclick="document.getElementById('del-cr-<?php echo $resultat->idCompteRendu ?>').submit()">Supprimer</a>
-                        </td>
-                    </tr>
-                    <form action="compte-rendu.php" method="post"
-                          id="edit-cr-<?php echo $resultat->idCompteRendu ?>">
-                        <input type="hidden" value="<?php echo $resultat->idCompteRendu ?>" name="idCR">
-                        <input type="hidden" value="<?php echo $resultat->idFormation ?>" name="formation">
-                        <input type="hidden" value="<?php echo $resultat->idPromo ?>" name="promotion">
-                        <input type="hidden" value="<?php echo $resultat->idModule ?>" name="module">
-                    </form>
-                    <form action="" method="post"
-                          id="del-cr-<?php echo $resultat->idCompteRendu ?>">
-                        <input type="hidden" value="<?php echo $resultat->idCompteRendu ?>"
-                               name="del-compte-rendu">
-                    </form>
-                    <?php
-                }
-                $query->close(); ?>
-                </tbody>
-            </table>
-            <?php
-            $_SESSION['html'] = ob_get_contents();
-            ob_end_flush(); ?>
-            <form action="export-cr.php" method="post">
-                <input type="hidden" value="<?php echo $libelle ?>" name="libelle">
-                <input class="btn btn-primary" type="submit" value="Exporter en PDF">
-            </form>
-        </div>
-    </div>
-    <?php } ?>
-</body>
-</html>
+<?php include("session.php");if (isset($_POST['del-compte-rendu'])) {    if (!$bdd->query('DELETE FROM compterendu WHERE idCompteRendu = "' . $_POST['del-compte-rendu'] . '"')) {        printf("Erreur : %s\n", $bdd->error);        $alertDelFail = "Le compte rendu n'a pas pu être supprimé.";    } else {        $alertDelSuccess = "Le compte rendu a bien été supprimé.";    }} ?><!doctype html><html lang="fr"><head>    <meta charset="UTF-8">    <meta name="viewport"          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">    <meta http-equiv="X-UA-Compatible" content="ie=edge">    <link href="../css/bootstrap.css" rel="stylesheet">    <title>Cahier de Texte</title></head><body><?php include("navbar.php") ?><div class="container">    <h1 class="text-center mb-4">Cahier de Texte par Formations</h1>    <div class="row">        <div class="col-xl-auto mx-auto mb-3">            <form class="mb-3" action="" method="get" name="formations">                <div class="form-floating mb-3">                    <select class="form-select" name="formation" onchange="this.form.submit()">                        <option hidden selected>Sélectionner une formation</option>                        <?php //Requete + verification formation sélectionnée                        $query = $bdd->query('SELECT formations.idFormation, formations.libelle, formations.reference FROM formations                        INNER JOIN modules ON formations.idFormation = modules.idFormation                        GROUP BY libelle');                        while ($resultat = $query->fetch_object()) {                            echo '<option value="' . $resultat->idFormation . '"';                            if (isset($_GET['formation'])) {                                if ($_GET['formation'] == $resultat->idFormation) {                                    $libelle = $resultat->reference;                                    echo 'selected';                                }                            }                            echo '>' . $resultat->libelle . '</option>';                        }                        $query->close(); ?>                    </select>                    <label for="formation">Formation</label>                </div>            </form>            <form class="mb-3" action="" method="get" name="promotions">                <?php if (isset($_GET['formation'])) {                echo '<input type="hidden" name="formation" value="' . $_GET['formation'] . '">'; ?>                <div class="form-floating mb-3">                    <select class="form-select" name="promotion" onchange="this.form.submit()">                        <option selected value="">Promotions non clôturées</option>                        <?php //Requete + verification formation sélectionnée                        $query = $bdd->query('SELECT idPromo, libelle FROM promotions WHERE idFormation = ' . $_GET['formation'] . ' ORDER BY verrouillage, dateDebut, dateFin');                        while ($resultat = $query->fetch_object()) {                            ;                            echo '<option value="' . $resultat->idPromo . '"';                            if (isset($_GET['promotion'])) {                                if ($_GET['promotion'] == $resultat->idPromo) {                                    $libelle = $resultat->libelle;                                    echo 'selected';                                }                            }                            echo '>' . $resultat->libelle . '</option>';                        }                        $query->close();                        ?>                    </select>                    <label for="formation">Promotion</label>                </div>            </form>        </div>        <?php } if (isset($_GET['formation'])) { ?>        <div class="col-xl-auto mx-auto">            <?php            echo '<h2 class="text-center mb-3">Cahier de Texte de ' . $libelle . '</h2>';            ob_start() ?>            <table class="table table-striped border border-3 text-center">                <thead>                <tr>                    <th scope="col">Num. Sem.</th>                    <th scope="col">Module</th>                    <th scope="col">Formateur</th>                    <th scope="col">Date</th>                    <th scope="col">Durée</th>                    <th scope="col">Contenu</th>                    <th scope="col">Moyen</th>                    <th scope="col">Objectif</th>                    <th scope="col">Evaluation</th>                    <th scope="col">Distanciel</th>                    <th scope="col">Action</th>                </tr>                </thead>                <tbody>                <?php                if (empty($_GET['promotion'])) {                    $query = $bdd->query('SELECT idCompteRendu, modules.idFormation, promotions.idPromo, modules.idModule, modules.libelle as mLibelle, compterendu.duree, nom, prenom, date, contenu, moyen, objectif, evaluation, distanciel, verrouillage FROM ((compterendu                        INNER JOIN modules ON modules.idModule = compterendu.idModule)                        INNER JOIN users on users.idUser = compterendu.idUser)                        INNER JOIN promotions on compterendu.idPromo = promotions.idPromo                        WHERE modules.idFormation = ' . $_GET['formation'] . ' AND verrouillage != 1 ORDER BY date, dateEntree');                } else {                    $query = $bdd->query('SELECT idCompteRendu, idFormation, idPromo, modules.idModule, modules.libelle as mLibelle, compterendu.duree, nom, prenom, date, contenu, moyen, objectif, evaluation, distanciel FROM ((compterendu                        INNER JOIN modules ON modules.idModule = compterendu.idModule)                        INNER JOIN users on users.idUser = compterendu.idUser)                        WHERE idPromo = ' . $_GET['promotion'] . ' ORDER BY date, dateEntree');                }                while ($resultat = $query->fetch_object()) {                    $date = strtotime($resultat->date); ?>                    <tr>                        <th scope="row"><?php echo date('W', $date) ?></th>                        <td><?php echo $resultat->mLibelle ?></td>                        <td><?php echo $resultat->nom . ' ' . $resultat->prenom ?></td>                        <td><?php echo date('d/m/Y', $date) ?></td>                        <td><?php echo $resultat->duree ?>h</td>                        <td><?php echo $resultat->contenu ?></td>                        <td><?php echo $resultat->moyen ?></td>                        <td><?php echo $resultat->objectif ?></td>                        <td><?php echo $resultat->evaluation ?></td>                        <td>                            <?php                            if (!empty($resultat->distanciel)) {                                echo '✔';                            } ?>                        </td>                        <td id="a">                            <a href="#"                               onclick="document.getElementById('edit-cr-<?php echo $resultat->idCompteRendu ?>').submit()">Modifier</a>                            <a href="#"                               onclick="document.getElementById('del-cr-<?php echo $resultat->idCompteRendu ?>').submit()">Supprimer</a>                        </td>                    </tr>                    <form action="compte-rendu.php" method="post"                          id="edit-cr-<?php echo $resultat->idCompteRendu ?>">                        <input type="hidden" value="<?php echo $resultat->idCompteRendu ?>" name="idCR">                        <input type="hidden" value="<?php echo $resultat->idFormation ?>" name="formation">                        <input type="hidden" value="<?php echo $resultat->idPromo ?>" name="promotion">                        <input type="hidden" value="<?php echo $resultat->idModule ?>" name="module">                    </form>                    <form action="" method="post"                          id="del-cr-<?php echo $resultat->idCompteRendu ?>">                        <input type="hidden" value="<?php echo $resultat->idCompteRendu ?>"                               name="del-compte-rendu">                    </form>                    <?php                }                $query->close(); ?>                </tbody>            </table>            <?php            $_SESSION['html'] = ob_get_contents();            ob_end_flush(); ?>            <form action="export-cr.php" method="post">                <input type="hidden" value="<?php echo $libelle ?>" name="libelle">                <input class="btn btn-primary" type="submit" value="Exporter en PDF">            </form>        </div>    </div>    <?php } ?></body></html>
